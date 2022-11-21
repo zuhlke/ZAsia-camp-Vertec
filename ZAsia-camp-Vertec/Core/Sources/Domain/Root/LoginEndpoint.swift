e0f58472467d283e4e136b9a01222db2ae20a79f
Eugene <eugene_tanqiyuan@hotmail.com>
2022-11-21 14:48:14 +0700

    Create LoginEndpoint.

---
+++ ZAsia-camp-Vertec/Core/Sources/Domain/Root/LoginEndpoint.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Eugene Tan on 21/11/22.
+//
+
+import Foundation
+import Support
+
+struct LoginEndpoint: HTTPEndpoint {
+    
+    struct Input: Codable {
+        var username: String
+        var password: String
+    }
+    
+    func request(for input: Input) throws -> HTTPRequest {
+        try .post("/login", body: .json(input))
+    }
+    
+    func parse(_ response: HTTPResponse) throws -> Void {
+        
+    }
+    
+}
